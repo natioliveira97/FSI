722760a299f5ac6ad21961e4c92cc79fbab2cb49
Natalia <natioliveira97@hotmail.com>
2020-01-21 11:17:35 -0300

    Adicionado classificador perceptron

--- car_base.py
+++ car_base.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn import svm
+from sklearn.linear_model import Perceptron
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.multiclass import OneVsOneClassifier
 from sklearn.multiclass import OutputCodeClassifier
@@ -38,6 +39,9 @@ def main():
     test_classes = np.array(test_classes)[0]
 
     # Para o svc faz a abordagem one-vs-rest, one-vs-one e Error-Correcting Output-Codes
+
+    print("Usando o classificador svc:")
+
     ovr_clf_svc = OneVsRestClassifier(svm.SVC()).fit(training_atributes, training_classes)
     ovr_svc_prediction = ovr_clf_svc.predict(test_atributes)
     print("one_against_rest ",metrics.accuracy_score(test_classes,ovr_svc_prediction))
@@ -49,6 +53,21 @@ def main():
     eoc_clf_svc = OutputCodeClassifier(svm.SVC(), code_size =((2 ** (n_classes-1) -1)/n_classes)).fit(training_atributes, training_classes)
     eoc_svc_prediction = eoc_clf_svc.predict(test_atributes)
     print("error correcting code ",metrics.accuracy_score(test_classes,eoc_svc_prediction))
+
+
+
+    print("\nUsando o classificador perceptron")
+    ovr_clf_per = OneVsRestClassifier(Perceptron(tol=1e-3, random_state=0)).fit(training_atributes, training_classes)
+    ovr_per_prediction = ovr_clf_per.predict(test_atributes)
+    print("one_against_rest ",metrics.accuracy_score(test_classes,ovr_per_prediction))
+
+    ovo_clf_per = OneVsOneClassifier(Perceptron(tol=1e-3, random_state=0)).fit(training_atributes, training_classes)
+    ovo_per_prediction = ovo_clf_per.predict(test_atributes)
+    print("one_against_one ",metrics.accuracy_score(test_classes,ovo_per_prediction))
+
+    eoc_clf_per = OutputCodeClassifier(Perceptron(tol=1e-3, random_state=0), code_size =((2 ** (n_classes-1) -1)/n_classes)).fit(training_atributes, training_classes)
+    eoc_per_prediction = eoc_clf_per.predict(test_atributes)
+    print("error correcting code ",metrics.accuracy_score(test_classes,eoc_per_prediction))
 
 
 
